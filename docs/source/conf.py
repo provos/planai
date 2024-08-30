1ff3982053b6dd3beb854ebccde34b8605ff2a64
Niels Provos <provos@gmail.com>
2024-08-29 19:46:56 -0700

    fix: try to fix readthedocs import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
@@ -32,4 +33,5 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_static_path = ['_static']
