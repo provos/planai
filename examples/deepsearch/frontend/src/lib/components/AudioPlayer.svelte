e9040a298e027b83097ac41b7b79bcc9d009684a
Niels Provos <provos@gmail.com>
2025-02-02 10:47:50 -0800

    feat: add track info and links to audio player

--- examples/deepsearch/frontend/src/lib/components/AudioPlayer.svelte
+++ examples/deepsearch/frontend/src/lib/components/AudioPlayer.svelte
@@ -130,6 +130,17 @@ See the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International 
 			{/if}
 		</div>
 
+		<div class="track-info-panel">
+			<p class="track-info-text">{$currentTrack.info}</p>
+			<a href={$currentTrack.link} target="_blank" rel="noopener noreferrer" class="track-info-link">
+				Learn more
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block ml-1" viewBox="0 0 20 20" fill="currentColor">
+					<path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+					<path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+				</svg>
+			</a>
+		</div>
+
 		<div class="player-timeline">
 			<span class="time-display">{format(time)}</span>
 			<div
--- examples/deepsearch/frontend/src/lib/stores/trackStore.js
+++ examples/deepsearch/frontend/src/lib/stores/trackStore.js
@@ -4,22 +4,30 @@ export const tracks = [
     {
         src: 'https://sveltejs.github.io/assets/music/strauss.mp3',
         title: 'The Blue Danube Waltz',
-        artist: 'Johann Strauss'
+        artist: 'Johann Strauss',
+        info: 'One of the most famous waltzes ever written, composed in 1866.',
+        link: 'https://en.wikipedia.org/wiki/The_Blue_Danube'
     },
     {
         src: 'https://sveltejs.github.io/assets/music/holst.mp3',
         title: 'Mars, the Bringer of War',
-        artist: 'Gustav Holst'
+        artist: 'Gustav Holst',
+        info: 'First movement of The Planets suite, composed between 1914 and 1916.',
+        link: 'https://en.wikipedia.org/wiki/The_Planets'
     },
     {
         src: 'https://sveltejs.github.io/assets/music/satie.mp3',
         title: 'Gymnopédie no. 1',
-        artist: 'Erik Satie'
+        artist: 'Erik Satie',
+        info: 'A gentle, atmospheric piece that defined ambient music, published in 1888.',
+        link: 'https://en.wikipedia.org/wiki/Gymnop%C3%A9dies'
     },
     {
         src: 'https://sveltejs.github.io/assets/music/mozart.mp3',
         title: 'Requiem in D minor, K. 626 - III. Sequence - Lacrymosa',
-        artist: 'Wolfgang Amadeus Mozart'
+        artist: 'Wolfgang Amadeus Mozart',
+        info: 'Part of Mozart\'s final composition, left unfinished at his death in 1791.',
+        link: 'https://en.wikipedia.org/wiki/Requiem_(Mozart)'
     }
 ];
 
