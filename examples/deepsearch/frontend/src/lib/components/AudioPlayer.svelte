6d78e65dcfa0dca59a2b5a353c2a8bff4e7de6e8
Niels Provos <provos@gmail.com>
2025-02-02 10:51:18 -0800

    feat: enhance audio player track info display and responsiveness

--- examples/deepsearch/frontend/src/lib/components/AudioPlayer.svelte
+++ examples/deepsearch/frontend/src/lib/components/AudioPlayer.svelte
@@ -131,12 +131,31 @@ See the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International 
 		</div>
 
 		<div class="track-info-panel">
-			<p class="track-info-text">{$currentTrack.info}</p>
-			<a href={$currentTrack.link} target="_blank" rel="noopener noreferrer" class="track-info-link">
-				Learn more
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline-block ml-1" viewBox="0 0 20 20" fill="currentColor">
-					<path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-					<path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+			<p class="track-info-text hidden md:block">{$currentTrack.info}</p>
+			<a
+				href={$currentTrack.link}
+				target="_blank"
+				rel="noopener noreferrer"
+				class="track-info-link"
+			>
+				{#if !$currentTrack.info}
+					Learn more
+				{:else}
+					<span class="hidden md:inline">Learn more</span>
+					<span class="md:hidden">More info</span>
+				{/if}
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="ml-1 inline-block h-4 w-4"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+				>
+					<path
+						d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+					/>
+					<path
+						d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+					/>
 				</svg>
 			</a>
 		</div>
